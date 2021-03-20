3d58877f8f26ac0093114e3861be0dee206c1205
Anjiboss <nhu.nhu.face@gmail.com>
2021-03-21 03:49:55 +0900

    save data and fetch data from heroku app -> change GUI

--- js/flashCardScript.js
+++ js/flashCardScript.js
@@ -1,10 +1,13 @@
 const card = $("#holder");
 
-let curNum = 1;
+let curNum;
 let front = true;
+let curLevel;
 let words;
 function changeLevel(level) {
   curNum = 1;
+  curLevel = level.split(" ")[1];
+  console.log(curLevel);
   if (level !== "Chọn cấp độ Kanji") {
     fetch("json/" + level + ".json")
       .then((res) => res.json())
@@ -60,3 +63,69 @@ function showTheWord(curNum, front) {
     card.html(`<p class="rotated-p">${words[curNum].hanViet}</p>`);
   }
 }
+
+let commentBtnFlag = false;
+$("#comments").click(() => {
+  if (!commentBtnFlag) {
+    $(".comments-container").css("transform", "translate(0%)");
+    if (!curNum || !curLevel) {
+      $(".comments-container").html("Choose a level or a word first");
+    } else {
+      $(".comments-container").html("Loading...");
+      fetchComment(curLevel, curNum);
+    }
+    commentBtnFlag = true;
+  } else {
+    $(".comments-container").css("transform", "translate(100%)");
+    commentBtnFlag = false;
+  }
+});
+
+const fetchComment = (curLevel, curNum) => {
+  const table = $("<div></div>");
+  fetch(`http://localhost:5000/api/comments/${curLevel}/${curNum}`)
+    .then((res) => res.json())
+    .then((comments) => {
+      console.log(comments);
+      comments.forEach((comment) => {
+        table.append(`<p id="user">${comment.user}:
+          ${comment.comment}
+        </p>`);
+      });
+      $(".comments-container").html(
+        `<button id="post-comment" onclick="showForm()">Post Comment</div>`
+      );
+      $(".comments-container").append(table);
+    });
+};
+const showForm = () => {
+  $(".comments-container").html(`
+      <form id="post-comment-form">
+        <input type="text" id="username"placeholder="UserName">
+        <input type="text" id="comment"placeholder="Comment">
+        <input type="submit" value="Comment">
+      </form>
+    `);
+  $("#post-comment-form").submit((e) => {
+    e.preventDefault();
+    postComment(curLevel, curNum, $("#username").val(), $("#comment").val());
+  });
+};
+
+const postComment = (curLevel, curNum, userName, comment) => {
+  console.log("I am called");
+  fetch(`http://localhost:5000/api/comments/`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      userName: userName,
+      level: curLevel,
+      curNum: curNum,
+      comment: comment,
+    }),
+  })
+    .then((res) => res.text())
+    .then((text) => console.log(text));
+};
